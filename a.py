36e068bd1a6adb0c75d885e2db31ec1e54b602cd
xuming3 <xuming3@asiainfo.com>
2019-03-21 14:50:17 +0800

    Initial commit

---
+++ a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*-coding:utf-8-*-
+__author__ = 'simon'
+__date__ = '2019/3/21 11:27'
+for i in range(10):
+    print(i)
+
+
+def test1():
+    pass
+
+
+def test2():
+    pass
+
+
+def test3():
+    pass
+
+
+class Test:
+    # TODO 使用TODO方式
+    pass
