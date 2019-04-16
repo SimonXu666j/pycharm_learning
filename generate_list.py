aef627b85e12005c35d04b0ca746379a2b1e8663
SimonXu666j <1187819864@qq.com>
2019-04-16 23:46:44 +0800

    20190416

---
+++ generate_list.py
@@ -0,0 +1,20 @@
+# 列表生成式
+a = [i + 1 for i in range(10)]
+print(a)
+
+# 生成器
+a2 = (i for i in range(1000))  # 生成器
+print(a2)
+# 一个个生成
+print(next(a2))
+print(next(a2))
+print(next(a2))
+print(next(a2))
+
+a3 = (i for i in range(5))
+print(next(a3))
+print(next(a3))
+print(next(a3))
+print(next(a3))
+print(next(a3))
+# print(next(a3))   #  生成完了继续生成就报错StopIteration
